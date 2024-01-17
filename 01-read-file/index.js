42659db2f09d0dc9474600360b563aa79e68bee3
Polina <polinachka0603@gmail.com>
2024-01-17 19:18:48 +0300

    feat: 01-read-file completed

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const stream = fs.createReadStream(filePath, 'utf8');
+
+stream.on('data', (chunk) => {
+  console.log(chunk);
+});
+
+stream.on('error', (err) => {
+  console.error(err);
+});
