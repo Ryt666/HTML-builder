2e5776bceebc3ec98db0ed7825dd7aa14d1e1991
Polina <polinachka0603@gmail.com>
2024-01-25 13:00:12 +0300

    fix: correct copy and delete files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,33 +1,46 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function copyFolderContent(sourceFolderPath, folderPath) {
-  try {
-    await fs.mkdir(folderPath, { recursive: true });
-
-    const files = await fs.readdir(sourceFolderPath);
-
-    for (const file of files) {
-      const sourceFilePath = path.join(sourceFolderPath, file);
-      const filePath = path.join(folderPath, file);
-
-      const stats = await fs.stat(sourceFilePath);
-
-      if (stats.isFile()) {
-        await fs.copyFile(sourceFilePath, filePath);
-        console.log(`Coped file: ${file}`);
-      } else if (stats.isDirectory()) {
-        await copyFolderContent(sourceFilePath, filePath);
-      }
-    }
-
-    console.log('Coping completed');
-  } catch (err) {
-    console.error('ERROR WHEN COPING', err);
-  }
-}
-
 const sourceFolderPath = '04-copy-directory/files';
 const folderPath = '04-copy-directory/files-copy';
 
-copyFolderContent(sourceFolderPath, folderPath);
+async function copyFolderContent(source, path) {
+  try {
+    await fs.copyFile(source, path);
+    console.log(`File copied.`);
+  } catch (error) {
+    console.error(`ERROR:`, error);
+  }
+}
+
+async function updateDestinationFolder() {
+  try {
+    const files = await fs.readdir(sourceFolderPath);
+
+    try {
+      await fs.mkdir(folderPath);
+      console.log('Coping completed.');
+    } catch (Error) {
+
+    }
+
+    const existingFiles = await fs.readdir(folderPath);
+
+    await Promise.all(existingFiles.map(async (existingFile) => {
+      if (!files.includes(existingFile)) {
+        const filePathToRemove = path.join(folderPath, existingFile);
+        await fs.unlink(filePathToRemove);
+        console.log(`File ${existingFile} was deleted from ${folderPath}.`);
+      }
+    }));
+    await Promise.all(files.map(async (file) => {
+      const sourceFilePath = path.join(sourceFolderPath, file);
+      const filePath = path.join(folderPath, file);
+      await copyFolderContent(sourceFilePath, filePath);
+    }));
+  } catch (error) {
+    console.error('ERROR:', error);
+  }
+}
+
+updateDestinationFolder();
